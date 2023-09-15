c64f42797ca44119cfe10648e3cd886fc17b7cbf
Rally <raul.gheba15@yahoo.ro>
2023-09-15 14:54:39 +0300

    --all

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Intro() {
+  return (
+    <div>
+      <h1>Raul Gheba</h1>
+      <p>
+        Web development enthusiast, always eager to learn and surpass my
+        capabilities every day.
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="me.jpg" alt="me"></img>;
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <Skill name="HTML+CSS" emoji="💪" />
+      <Skill name="Sass" emoji="💪" />
+      <Skill name="Javascript" emoji="💪" />
+      <Skill name="OOP" emoji="💪" />
+    </ul>
+  );
+}
+
+function Skill(props) {
+  return (
+    <li className="skill">
+      <p>
+        {props.name} {props.emoji}
+      </p>
+    </li>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
